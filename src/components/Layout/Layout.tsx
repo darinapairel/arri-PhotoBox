8dcbbc6b20eb40f32f99a1a5c472388c56af7cef
Darina Pairel <d.pairel@marvel.ru>
2023-01-16 15:51:01 +0300

    add slider component: styles and logic

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,37 @@
 import React from 'react'
 import * as S from './styles'
 
+const photosList: {
+  id: number,
+  name: string,
+  src: string,
+  alt: string
+}[] = [
+	{
+		id: 1,
+		name: '',
+		src: 'https://play-lh.googleusercontent.com/jkpabs01pnEU5Jc9U3MuWdwwoWi8v7x33RZNYyLP2T8a2j1csnjOy3_-KI6JU8JntlNW',
+		alt: ''
+	},
+	{
+		id: 2,
+		name: '',
+		src: '',
+		alt: ''
+	},
+	{
+		id: 3,
+		name: '',
+		src: '',
+		alt: ''
+	},
+]
+
 export const Layout = () => {
 	return (
 		<>
 			<S.HeaderNav />
-			<S.Content />
+			<S.Content photos={photosList} />
 			<S.Footer
 				info={{
 					date: `${new Date('06 02 2022')}`, //add mask for date
--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,9 +1,52 @@
 import React from 'react'
 import * as S from './styles'
 
-export const Slider = () => {
+type Photo = {
+  photos: {
+  id: number,
+  name: string,
+  src: string,
+  alt: string
+}[]}
+// type Photos = {[key:string]: Photo}
+
+export const Slider: React.FC<Photo> = ({photos}) => {
+  const slideRef = React.createRef<HTMLLIElement>()
+  const slidesContainerRef = React.createRef<HTMLUListElement>()
+  
+  // SLIDER CLICK LOGIC 
+  const btnClkHandler = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, next=false) => {
+    const slideWidth = slideRef.current ? slideRef.current.clientWidth : 0
+    console.log('event', typeof event, '\nref', slideRef.current, next, slideWidth)
+    if (slidesContainerRef.current)
+      next ?
+        slidesContainerRef.current.scrollLeft += slideWidth
+        :
+        slidesContainerRef.current.scrollLeft -= slideWidth
+  }
   return (
-   <>
-   </>
+    <S.SliderWrapper className="slider-wrapper">
+      {/* control buttons */}
+      <S.SlideArrow className="slide-arrow" id="slide-arrow-prev" onClick={(event)=> btnClkHandler(event)}>
+        &#8249;
+      </S.SlideArrow>
+      <S.SlideArrow className="slide-arrow" id="slide-arrow-next" onClick={(event)=> btnClkHandler(event, true)}>
+        &#8250;
+      </S.SlideArrow>
+      {/* list of Slides (photos)*/}
+      <S.SlidesContainer className="slides-container" id="slides-container" ref={slidesContainerRef}>
+        {
+          photos.map((photo) => {
+            return(
+              <S.Slide className="slide" ref={slideRef}>
+                <S.ImgPhoto src={photo.src} alt={photo.alt} />
+              </S.Slide>
+            )
+          }
+          )
+        }
+      </S.SlidesContainer>
+    </S.SliderWrapper>
   )
+   
 }
