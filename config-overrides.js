f23492c0369ab133397c99a47ea4e5fa7ca5a8c7
Darina Pairel <d.pairel@marvel.ru>
2023-06-27 15:23:56 +0300

    add configs

---
+++ config-overrides.js
@@ -0,0 +1,7 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+// Overrides create-react-app webpack configs without ejecting
+// https://github.com/timarney/react-app-rewired
+/*
+const { useBabelRc, override } = require("customize-cra");
+module.exports = override(useBabelRc());
+*/
