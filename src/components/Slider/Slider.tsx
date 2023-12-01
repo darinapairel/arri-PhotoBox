dc1f397a109a0b5410c8c0466360b3904b52781d
Darina Pairel <d.pairel@marvel.ru>
2023-12-01 10:53:29 +0300

    arrows and handlers update

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,52 +1,80 @@
-import React from 'react'
-import * as S from './styles'
+import React, { useState } from "react";
+
+import * as S from "./styles";
 
 type Photo = {
   photos: {
-  id: number,
-  name: string,
-  src: string,
-  alt: string
-}[]}
-// type Photos = {[key:string]: Photo}
+    id: number;
+  } & { [k: string]: string }[];
+};
 
-export const Slider: React.FC<Photo> = ({photos}) => {
-  const slideRef = React.createRef<HTMLLIElement>()
-  const slidesContainerRef = React.createRef<HTMLUListElement>()
-  
-  // SLIDER CLICK LOGIC 
-  const btnClkHandler = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, next=false) => {
-    const slideWidth = slideRef.current ? slideRef.current.clientWidth : 0
-    console.log('event', typeof event, '\nref', slideRef.current, next, slideWidth)
-    if (slidesContainerRef.current)
-      next ?
-        slidesContainerRef.current.scrollLeft += slideWidth
-        :
-        slidesContainerRef.current.scrollLeft -= slideWidth
-  }
+export const Slider: React.FC<Photo> = ({ photos }) => {
+  const slideRef = React.createRef<HTMLLIElement>();
+  const slidesContainerRef = React.createRef<HTMLUListElement>();
+
+  const [currentId, setCurrentSlide] = useState(1);
+
+  // SLIDER CLICK LOGIC
+  const btnClkHandler = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    const slideWidth = slideRef?.current ? slideRef.current.clientWidth : 0;
+    const isNext = (event.target as HTMLElement).id.lastIndexOf("next") > -1;
+    const setNext = () => {
+      if (slidesContainerRef.current) {
+        slidesContainerRef.current.scrollLeft += slideWidth;
+        setCurrentSlide(currentId + 1);
+      }
+      return slidesContainerRef?.current?.scrollLeft;
+    };
+
+    const setPrev = () => {
+      if (slidesContainerRef.current) {
+        slidesContainerRef.current.scrollLeft -= slideWidth;
+        setCurrentSlide(currentId - 1);
+      }
+      return slidesContainerRef?.current?.scrollLeft;
+    };
+
+    isNext ? setNext() : setPrev();
+
+    console.log();
+  };
+
   return (
     <S.SliderWrapper className="slider-wrapper">
       {/* control buttons */}
-      <S.SlideArrow className="slide-arrow" id="slide-arrow-prev" onClick={(event)=> btnClkHandler(event)}>
-        &#8249;
-      </S.SlideArrow>
-      <S.SlideArrow className="slide-arrow" id="slide-arrow-next" onClick={(event)=> btnClkHandler(event, true)}>
-        &#8250;
-      </S.SlideArrow>
+      {currentId > 1 && (
+        <S.SlideArrow
+          className="slide-arrow"
+          id="slide-arrow-prev"
+          onClick={btnClkHandler}
+        >
+          &#8249;
+        </S.SlideArrow>
+      )}
+      {currentId < photos.length && (
+        <S.SlideArrow
+          className="slide-arrow"
+          id="slide-arrow-next"
+          onClick={btnClkHandler} // tru
+        >
+          &#8250;
+        </S.SlideArrow>
+      )}
       {/* list of Slides (photos)*/}
-      <S.SlidesContainer className="slides-container" id="slides-container" ref={slidesContainerRef}>
-        {
-          photos.map((photo) => {
-            return(
-              <S.Slide className="slide" ref={slideRef}>
-                <S.ImgPhoto src={photo.src} alt={photo.alt} />
-              </S.Slide>
-            )
-          }
-          )
-        }
+
+      <S.SlidesContainer
+        className="slides-container"
+        id="slides-container"
+        ref={slidesContainerRef}
+      >
+        {photos.map(({ src, alt, id }) => {
+          return (
+            <S.Slide key={id} className="slide" ref={slideRef}>
+              <S.ImgPhoto src={src} alt={alt} />
+            </S.Slide>
+          );
+        })}
       </S.SlidesContainer>
     </S.SliderWrapper>
-  )
-   
-}
+  );
+};
